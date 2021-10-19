12fae0952e4f2f0d29d46b096129393d756bb894
petukhov-ns <petukhov-ns@so-ups.ru>
2021-10-19 17:09:44 +0300

    gitignore

--- common/src/main/java/jdev/dto/Main.java
+++ common/src/main/java/jdev/dto/Main.java
@@ -1,11 +1,16 @@
 package jdev.dto;
 
+<<<<<<< HEAD
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String... args) throws JsonProcessingException, InterruptedException {
+=======
+public class Main {
+    public static void main(String... args) throws Exception {
+>>>>>>> 2c65db014be42eee58eb589604ef82cece6f130e
         for (int i=0; i<5; i++) {
             System.out.println("COMMON say Hello!!!!");
             PointDTO point = new PointDTO();
