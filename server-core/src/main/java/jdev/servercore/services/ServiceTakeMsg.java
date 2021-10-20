2905e0cf942f6260518241acb521baa34b474fb9
petukhov-ns <petukhov-ns@so-ups.ru>
2021-10-20 15:29:49 +0300

    Добавлен CRUD for server-core

--- server-core/src/main/java/jdev/servercore/services/ServiceTakeMsg.java
+++ server-core/src/main/java/jdev/servercore/services/ServiceTakeMsg.java
@@ -1,9 +1,13 @@
 package jdev.servercore.services;
 
+import jdev.servercore.db.PointDevice;
+import jdev.servercore.db.repo.PointDeviceRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -12,7 +16,8 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@EnableJpaRepositories("jdev.servercore.db")
+@EntityScan(basePackageClasses = jdev.servercore.db.PointDevice.class)
 @Service
 public class ServiceTakeMsg {
     private static final Logger LOG_COORDS = LoggerFactory.getLogger("takeCoords.ServerCore");
@@ -24,6 +29,9 @@ public class ServiceTakeMsg {
     public ServiceTakeMsg(@Autowired RestTemplate restTemplate){
         this.restTemplate = restTemplate;
     }
+
+    @Autowired
+    PointDeviceRepository pointDeviceRepository;
 
     @Scheduled(cron = "${takeFromServer.cron}")
     public List<String> takeQueueMsg(){
@@ -38,7 +46,9 @@ public class ServiceTakeMsg {
 
             LOG_TRACE.trace(String.valueOf("Передано " + listOfPoints.size() + " координат."));
             listOfPoints.stream()
-                    .forEach(coord -> LOG_COORDS.info(coord));
+                    .forEach(coord -> {
+                        create(System.currentTimeMillis(), 1, coord);
+                        LOG_COORDS.info(coord);});
 
         }
         catch (HttpClientErrorException e)
@@ -51,5 +61,55 @@ public class ServiceTakeMsg {
         }
 
         return listOfPoints;
+    }
+
+/*****************************************************************/
+/**Работа с БД****************************************************/
+/*****************************************************************/
+    /**
+     * Create
+     */
+    private PointDevice create(
+                            long timePoint,
+                            int idDevice,
+                            String coord){
+        PointDevice pointDevice = new PointDevice(
+                timePoint,
+                idDevice,
+                coord
+        );
+        return pointDeviceRepository.save(pointDevice);
+    }
+    /**
+     * Read
+     */
+    private void read() {
+        List<PointDevice> pointDevicesList;
+        pointDevicesList = (List<PointDevice>) pointDeviceRepository.findAll();
+
+        if (pointDevicesList.size() == 0) {
+            LOG_TRACE.trace("NO RECORDS");
+        } else {
+            pointDevicesList.stream().forEach(rocket -> LOG_TRACE.trace(rocket.toString()));
+        }
+    }
+    /**
+     * Update
+     */
+    private void update(PointDevice pointDevice,
+                        long timePoint,
+                        int idDevice,
+                        String coord) {
+        pointDevice = new PointDevice(
+                timePoint,
+                idDevice,
+                coord);
+        pointDeviceRepository.save(pointDevice);
+    }
+    /**
+     * Delete
+     */
+    private void delete(PointDevice pointDevice) {
+        pointDeviceRepository.delete(pointDevice);
     }
 }
